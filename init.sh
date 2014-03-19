db50ea4c10bd5ff0160453d260f94f13c0183bb1
aaron <aaron@ubuntu.(none)>
2014-03-19 17:16:01 +0800

    Add inputrc dotfile

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 
-apt-get install zsh tmux vim
+if [ $(id -u) != 0 ]; then
+    echo "Warning: Please run as administrator"
+    exit 0
+fi
+
+apt-get install -y zsh tmux vim > /dev/null
 
 DOTFOLDER="$HOME/.dotfiles"
 DOTLIST=$(ls | egrep -v "(init.sh|README.md)")
@@ -10,8 +15,10 @@ test ! -d $DOTFOLDER && mkdir $DOTFOLDER
 test ! -d $BACKUPFOLDER && mkdir $BACKUPFOLDER
 
 for dotfile in $DOTLIST
-do 
+do
+    echo "Move .$dotfile from $HOME to $BACKUPFOLDER" 
     mv $HOME/.$dotfile $BACKUPFOLDER/$dotfile
+    echo "Create symbolic link .$dotfile at $HOME linking to $BACKUPFOLDER/$dotfile"
     ln -s $(pwd)/$dotfile $HOME/.$dotfile
 done
 
